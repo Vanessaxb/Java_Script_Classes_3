cc9cad2f6d9307a2403aca6c2af752fcd8648cbe
Vanessa <vanessambeattie@gmail.com>
2023-07-11 13:45:43 -0500

    update

---
+++ script.js
@@ -0,0 +1,23 @@
+//  Hard Mode (Practice Command Line, recall Git )
+
+//* Hamster
+class Hamster {
+    constructor(owner, name, price){
+    this.owner = ""; 
+    this.name; 
+    this.price = 15
+
+}
+
+wheelRun() {
+    console.log("squeak squeak");}
+
+eatFood() {
+    console.log("nibble nibble")
+}
+
+getPrice() {
+    console.log(this.price)
+}
+}
+
